822a02fea6c2f6bc33543b881d354a80640ca1fe
unknown <polovnikovai19i@gmail.com>
2023-10-06 11:57:31 +0300

    изменение порта на 3001

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const handleError = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/mestodb" } = process.env;
+const { PORT = 3001, DB_URL = "mongodb://127.0.0.1:27017/mestodb" } = process.env;
 
 const app = express();
 
